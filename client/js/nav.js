9e423eee189eb45e90d533b23d53d3882f7047a2
blkt <56833452+bl-kt@users.noreply.github.com>
2022-03-08 14:52:59 +0000

    Navbar almost done

--- client/js/nav.js
+++ client/js/nav.js
@@ -1,8 +1,17 @@
+
 document.addEventListener('DOMContentLoaded', async () => {
   const res = await fetch('./components/nav.html');
   const nav = await res.text();
   const wrapper = document.querySelector('#navWrapper');
   wrapper.innerHTML = nav;
+
+  const menuToggle = document.querySelector('#menuToggle');
+  const navMenu = document.querySelector('#navMenu');
+
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    console.log('Menu');
+  });
 });
 
-document.addEventListener((document.querySelector('menu-toggle')));
