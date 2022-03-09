c506ee348fe404a69ee0eab3915634b3a0cfb2af
blkt <char.ryder@blkt.co.uk>
2022-03-09 18:13:04 +0000

    Mostly finished querybar

--- client/js/sortFilter.js
+++ client/js/sortFilter.js
@@ -1,30 +1,8 @@
-import { colours, dimensions} from './data/filters.json';
-
-function populateFilter() {
-    // loop through filter list
-        // per colour, add li to ul
-        // add event listener for when click, export filter name
-};
-
-function populateSort() {
-
-};
-
-function filter() {
-    // takes filter param
-    // filter
-};
-
-function sort() {
-    // gets sort param
-    // sorts
-};
 
 // renders filter bar
 document.addEventListener('DOMContentLoaded', async () => {
-    const res = await fetch('./components/nav.html');
-    const querybar = await res.text();
-    const wrapper = document.querySelector('#queryWrapper');
-    wrapper.innerHTML = querybar;
+  const res = await fetch('./components/sortFilter.html');
+  const querybar = await res.text();
+  const wrapper = document.querySelector('#queryWrapper');
+  wrapper.innerHTML = querybar;
 });
-
